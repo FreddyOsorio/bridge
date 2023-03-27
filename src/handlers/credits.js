38f11939d7f0ba875b9f938353bb31ff3a0ccf5b
FreddyOsorio <freddyamaury@gmail.com>
2023-03-27 15:47:28 -0600

    primera versión de bridge para Mock

---
+++ src/handlers/credits.js
@@ -0,0 +1,184 @@
+import {
+  beginActionExisting,
+  beginActionNew,
+  endAction,
+  saveIntent,
+} from './common.js'
+import { transactionWrapper, updateEntry } from '../persistence.js'
+import { ledgerSigner, notifyLedger } from '../ledger.js'
+import {
+  extractAndValidateData,
+  validateAction,
+  validateEntity,
+} from '../validators.js'
+import core from '../core.js'
+
+export async function prepareCredit(req, res) {
+  req.body.data.schema = 'credit'
+  //req.body.schema = 'credit'
+  const action = 'prepare-credit'
+
+  // Begin Action processing for new Entry which will also save it.
+  let { alreadyRunning, entry } = await beginActionNew({
+    request: req,
+    action,
+  })
+
+  // The Entry is already saved, so we can return 202 Accepted
+  // to Ledger so that it stops redelivering the Action.
+  res.sendStatus(202)
+
+  // If the Action is already being processed, skip processing.
+  if (!alreadyRunning) {
+    await processPrepareCredit(entry)
+
+    // Stop Action processing and save the result.
+    await endAction(entry)
+  }
+
+  // If Entry is in final state, return the result to Ledger
+  await notifyLedger(entry, action, ['prepared', 'failed'])
+}
+
+async function processPrepareCredit(entry) {
+  const action = entry.actions[entry.processingAction]
+  try {
+    // Parse data from the Entry and validate it.
+    validateEntity(
+      { hash: entry.hash, data: entry.data, meta: entry.meta },
+      ledgerSigner,
+    )
+    validateEntity(entry.data?.intent)
+    validateAction(action.action, entry.processingAction)
+
+    const { address, symbol, amount } = extractAndValidateData({
+      entry,
+      schema: 'credit',
+    })
+
+    // Save extracted data into Entry, we will need this for other Actions.
+    entry.schema = 'credit'
+    entry.account = address.account
+    entry.symbol = symbol
+    entry.amount = amount
+
+    // Save Entry.
+    await transactionWrapper(async (client) => {
+      await updateEntry(client, entry)
+    })
+
+    //QIK_COMMENT Se debe llamar al MS Transferencias http://localhost:3000/credits/prepare-credit 
+
+    // Save Intent from Entry.
+    await saveIntent(entry.data.intent)
+
+    // Processing prepare Action for Credit Entry in the core is simple and
+    // only checks if the account exists and is active. If something is wrong,
+    // an Error will be thrown, and we will catch it later.
+    const coreAccount = core.getAccount(Number(entry.account))
+    coreAccount.assertIsActive()
+
+    action.state = 'prepared'
+  } catch (error) {
+    //QIK_COMMENT Se debe llamar al MS Transferencias http://localhost:3000/credits/abort-credit con el disparador prepare-credit 
+    console.log(error)
+    action.state = 'failed'
+    action.error = {
+      reason: 'bridge.unexpected',
+      detail: error.message,
+      failId: undefined,
+    }
+  }
+}
+
+export async function commitCredit(req, res) {
+  const action = 'commit-credit'
+  let { alreadyRunning, entry } = await beginActionExisting({
+    request: req,
+    action,
+    previousStates: ['prepared'],
+  })
+
+  res.sendStatus(202)
+
+  if (!alreadyRunning) {
+    await processCommitCredit(entry)
+    await endAction(entry)
+  }
+
+  await notifyLedger(entry, action, ['committed'])
+}
+
+async function processCommitCredit(entry) {
+  const action = entry.actions[entry.processingAction]
+  let transaction
+  try {
+    validateEntity(
+      { hash: action.hash, data: action.data, meta: action.meta },
+      ledgerSigner,
+    )
+    validateAction(action.action, entry.processingAction)
+      //QIK_COMMENT Se debe llamar al MS Transferencias http://localhost:3000/credits/commit-credit 
+    transaction = core.credit(
+      Number(entry.account),
+      entry.amount,
+      `${entry.handle}-credit`,
+    )
+    action.coreId = transaction.id
+
+    if (transaction.status !== 'COMPLETED') {
+      throw new Error(transaction.errorReason)
+    }
+
+    action.state = 'committed'
+  } catch (error) {
+    //QIK_COMMENT Se debe llamar al MS Transferencias http://localhost:3000/credits/abort-credit  con el disparador commit-credit
+    console.log(error)
+    action.state = 'error'
+    action.error = {
+      reason: 'bridge.unexpected',
+      detail: error.message,
+      failId: undefined,
+    }
+  }
+}
+
+export async function abortCredit(req, res) {
+  const action = 'abort-credit'
+  let { alreadyRunning, entry } = await beginActionExisting({
+    request: req,
+    action,
+    previousStates: ['prepared', 'failed'],
+  })
+
+  res.sendStatus(202)
+
+  if (!alreadyRunning) {
+    await processAbortCredit(entry)
+    await endAction(entry)
+  }
+
+  await notifyLedger(entry, action, ['aborted'])
+}
+
+async function processAbortCredit(entry) {
+  const action = entry.actions[entry.processingAction]
+  try {
+    validateEntity(
+      { hash: action.hash, data: action.data, meta: action.meta },
+      ledgerSigner,
+    )
+    validateAction(action.action, entry.processingAction)
+
+    //QIK_COMMENT Se debe llamar al MS Transferencias http://localhost:3000/credits/abort-credit con el disparador abort-credit 
+    action.state = 'aborted'
+  } catch (error) {
+    console.log(error)
+    action.state = 'error'
+    action.error = {
+      reason: 'bridge.unexpected',
+      detail: error.message,
+      failId: undefined,
+    }
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import { logRequest } from './middleware/logging.js'
+import { asyncErrorWrapper, handleErrors } from './middleware/errors.js'
+import { abortCredit, commitCredit, prepareCredit } from './handlers/credits.js'
+import { abortDebit, commitDebit, prepareDebit } from './handlers/debits.js'
+import { updateIntent } from './handlers/intents.js'
+
+import * as persistence from './persistence.js'
+
+process.on('exit', async () => {
+  await persistence.shutdown()
+})
+
+await persistence.init()
+
+const bankName = 'Demo bank Qik'
+const port = 3001
+
+const app = express()
+
+app.use(express.json())
+
+app.use(logRequest)
+
+app.get('/', (req, res) => {
+  res.send(`${bankName} is running!`)
+})
+
+app.post('/credits', asyncErrorWrapper(prepareCredit))
+
+
+app.post('/credits/:handle/commit', asyncErrorWrapper(commitCredit))
+app.post('/credits/:handle/abort', asyncErrorWrapper(abortCredit))
+
+app.post('/debits', asyncErrorWrapper(prepareDebit))
+app.post('/debits/:handle/commit', asyncErrorWrapper(commitDebit))
+app.post('/debits/:handle/abort', asyncErrorWrapper(abortDebit))
+
+app.put('/intents/:handle', asyncErrorWrapper(updateIntent))
+
+app.use(handleErrors)
+
+app.listen(port, () => {
+  console.log(`${bankName} running on port ${port}`)
+})
